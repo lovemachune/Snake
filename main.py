e552951476c814554b4822287d70803b00bff1bc
lovemachune <lovemachine861005@gmail.com>
2020-09-04 17:25:19 +0800

    add socket

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import sys
 from pygame.locals import QUIT
 from draw import *
 import random
+import socket
+import threading
+
+alive = True
+HOST = '127.0.0.1'
+PORT = 8888
+read = ''
 
 class fruits:
     def __init__(self):
@@ -33,12 +40,34 @@ def check_overlap(snakes, fruit):
         snakes.add_spped()
         snakes.add_len([fruit_loc[0], fruit_loc[1]])
         return True
-    return  False
+    return False
 
-def hide():
-    print("tttt")
+def mysocket():
+    global read, alive
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(10)
+    conn, _ = server.accept()
+    while alive:
+        read = conn.recv(1024).decode('ascii')
+        if not read:
+            break
+        read = read.replace('\n', '')
+    conn.close()
 
-def main():
+def check_client(snakes):
+    global read
+    if read == 'w':
+        snakes.set_dir(2)
+    if read == 'a':
+        snakes.set_dir(-1)
+    if read == 's':
+        snakes.set_dir(-2)
+    if read == 'd':
+        snakes.set_dir(1)
+
+def main(thread):
+    global read, alive
     pygame.init()
     frame_count = 0
     screen = pygame.display.set_mode((screen_width, screen_height))
@@ -50,24 +79,35 @@ def main():
     main_clock = pygame.time.Clock()
     fruit = fruits()
     game_flag = True
-    while True:
+    while alive:
+        if not thread.is_alive():
+            thread = threading.Thread(target=mysocket)
+            thread.start()
         main_clock.tick(fps)
         for event in pygame.event.get():
             # 當使用者結束視窗，程式也結束
             if event.type == QUIT:
+                alive = False
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     snakes.set_dir(2)
+                    read = 'w'
                 if event.key == pygame.K_a:
                     snakes.set_dir(-1)
+                    read = 'a'
                 if event.key == pygame.K_s:
                     snakes.set_dir(-2)
+                    read = 's'
                 if event.key == pygame.K_d:
                     snakes.set_dir(1)
-                if event.key == pygame.K_r and game_flag == False:
-                    main()
+                    read = 'd'
+                if event.key == pygame.K_r and not game_flag:
+                    main(thread)
+        if not game_flag and read == 'r':
+            main(thread)
+        check_client(snakes)
         if snakes.game_over():
             game_over = snakes.get_font().render("GAME OVER", True, red)
             restart = snakes.get_font().render("Press R to restart", True, red)
@@ -87,6 +127,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    thread = threading.Thread(target=mysocket)
+    thread.start()
+    main(thread)
+
 
 
